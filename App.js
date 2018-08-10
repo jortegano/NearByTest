ce4049c8ed2103b0b2a03aecc784e4bd29ce0a67
Jose Ortegano <jose@devtence.com>
2018-08-10 13:05:16 -0500

    Adding https://github.com/MacKentoch/react-native-beacons-manager

--- App.js
+++ App.js
@@ -12,8 +12,10 @@ import {
   View,
   TouchableOpacity,
   ScrollView,
+  AppState,
 } from "react-native";
 import { NearbyAPI } from "react-native-nearby-api";
+import Beacons from 'react-native-beacons-manager';
 
 const instructions = Platform.select({
   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
@@ -27,6 +29,16 @@ const nearbyAPI = new NearbyAPI(true);
 const API_KEY = "AIzaSyCbr07ltdESJFUFWNF9SejR1SkdfoJhl84";
 
 export default class App extends Component {
+
+  /*react-native-beacons-manager*/
+  // will be set as a reference to "regionDidEnter" event:
+  regionDidEnterEvent = null;
+  // will be set as a reference to "regionDidExit" event:
+  regionDidExitEvent = null;
+  uuid = '00000000-0000-0000-0001-000000000022';//'7b44b47b-52a1-5381-90c2-f09b6838c5d4',
+  identifier = 'CRF_REGION';
+  /**/
+
   constructor() {
     super();
     this.state = {
@@ -83,6 +95,8 @@ export default class App extends Component {
         });
       });
     });
+
+    this.scanWithBeaconManager();
   }
 
   _connectPress = () => {
@@ -112,6 +126,61 @@ export default class App extends Component {
       });
     }
   };
+
+  scanWithBeaconManager = () => {
+    if(Platform.OS === 'ios'){
+      console.log('###### ScanWithBeaconManager');
+      Beacons.requestAlwaysAuthorization();
+      Beacons.allowsBackgroundLocationUpdates(true);
+      Beacons.shouldDropEmptyRanges(true);
+      const region = { identifier: this.identifier, uuid: this.uuid };
+      console.log('region data ->', region);
+
+      Beacons
+      .startMonitoringForRegion(region) // or like  < v1.0.7: .startRangingBeaconsInRegion(identifier, uuid)
+      .then(() => {
+        console.log('Beacons monitoring started succesfully')
+        // start monitoring:
+        this.regionDidEnterEvent = Beacons.BeaconsEventEmitter.addListener(
+          'regionDidEnter',
+          (data) => {
+            console.log('monitoring - regionDidEnter data: ', data);
+          }
+        );
+ 
+        this.regionDidExitEvent = Beacons.BeaconsEventEmitter.addListener(
+          'regionDidExit',
+          ({ identifier, uuid, minor, major }) => {
+            console.log('monitoring - regionDidExit data: ', { identifier, uuid, minor, major });
+          }
+        );
+      }).catch(error => console.log(`Beacons monitoring not started, error: ${error}`));
+
+      Beacons.startUpdatingLocation();
+      
+    }
+  }
+
+  stopScanningWithBeaconManager = () => {
+    if(Platform.OS === 'ios'){
+      Beacons
+      .stopMonitoringForRegion(region)
+      .then(() => console.log('Beacons monitoring stopped succesfully'))
+      .catch(error => console.log(`Beacons monitoring not stopped, error: ${error}`));
+      // stop updating locationManager:
+      Beacons.stopUpdatingLocation();
+      // remove auth state event we registered at componentDidMount:
+      this.authStateDidRangeEvent.remove();
+      // remove monitiring events we registered at componentDidMount::
+      this.regionDidEnterEvent.remove();
+      this.regionDidExitEvent.remove();
+    }
+  }
+
+  componentWillUnMount() {
+    // stop monitoring beacons:
+    this.stopScanningWithBeaconManager();
+  }
 
   render() {
     return (
